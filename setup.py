e9b7289c65cfc82137bd8d64d230bfa4782d159a
Dean Gardiner <me@dgardiner.net>
2016-06-20 01:16:37 +1200

    Updated [setup.py] to use pbr

--- setup.py
+++ setup.py
@@ -2,15 +2,6 @@ from setuptools import setup
 
 
 setup(
-    name='oem-format-minimize-msgpack',
-    version='1.0.0',
-
-    author="Dean Gardiner",
-    author_email="me@dgardiner.net",
-
-    install_requires=[
-        'oem-framework>=1.0.0',
-        'oem-format-minimize>=1.0.0',
-        'oem-format-msgpack>=1.0.0'
-    ]
+    setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
+    pbr=True,
 )
